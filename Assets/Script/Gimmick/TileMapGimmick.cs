476d658e76f41fb12f9c49b152c30f496927db21
kousuke kurose <kousukemanchotkuro@gmail.com>
2026-02-25 14:53:03 +0900

    TileMapGimmickの地面生成を追加

--- Assets/Script/Gimmick/TileMapGimmick.cs
+++ Assets/Script/Gimmick/TileMapGimmick.cs
@@ -6,23 +6,27 @@ public enum GimmickType { Erase, Create, Blink }
 
 public class TileMapGimmick : MonoBehaviour
 {
+    public TileBase tileToPlace;
     public Tilemap tilemap;
     //インスペクターでモード切り替え
     public GimmickType type;
     //消す範囲をインスペクターで設定
     public int width = 5;
     public int height = 5;
+    private Vector3Int originCell;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
+            originCell = tilemap.WorldToCell(transform.position);
             switch (type)
             {
                 case GimmickType.Erase:
                     EraseArea();
                     break;
                 case GimmickType.Create:
+                    CreateArea();
                     break;
                 case GimmickType.Blink:
                     break;
@@ -36,17 +40,32 @@ public class TileMapGimmick : MonoBehaviour
     //タイルマップを消す
     public void EraseArea()
     {
-        Vector3Int originCelll = tilemap.WorldToCell(transform.position);
+        //originCell = tilemap.WorldToCell(transform.position);
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
                 //中央から広げたり、下方向に伸ばしたり自由に計算
-                Vector3Int tragetPos = new Vector3Int(originCelll.x + x, originCelll.y - y, 0);
+                Vector3Int tragetPos = new Vector3Int(originCell.x + x, originCell.y - y, 0);
                 tilemap.SetTile(tragetPos, null);
             }
         }
         //一度発動したらトリガーを消す
+        Destroy(gameObject);
+    }
+
+    public void CreateArea()
+    {
+        //originCell = tilemap.WorldToCell(transform.position);
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                //中央から広げたり、下方向に伸ばしたり自由に計算
+                Vector3Int tragetPos = new Vector3Int(originCell.x + x, originCell.y - y, 0);
+                tilemap.SetTile(tragetPos, tileToPlace);
+            }
+        }
         Destroy(gameObject);
     }
 }
